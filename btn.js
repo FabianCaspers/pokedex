8ccf106daf6579ee504a85937117ff471922d1ed
FabianCaspers <119706742+FabianCaspers@users.noreply.github.com>
2023-01-17 22:13:04 +0100

    bugfix

--- btn.js
+++ btn.js
@@ -1,13 +1,29 @@
 // Get the button:
 let mybutton = document.getElementById("loadMore");
+let clicked = false;
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
 
 
   if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    mybutton.style.display = "block";
+    if(!clicked){
+      mybutton.style.display = "block";
+    }
   } else {
     mybutton.style.display = "none";
+    clicked = false;
   }
-}
+};
+
+mybutton.addEventListener("click", function() {
+  clicked = true;
+  mybutton.style.display = "none";
+});
+
+
+
+
+
+
+
