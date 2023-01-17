6af8700ffc157379bd1bd5b7dd47c43b96afd8a0
FabianCaspers <119706742+FabianCaspers@users.noreply.github.com>
2023-01-17 21:55:34 +0100

    bugfix

--- btn.js
+++ btn.js
@@ -2,10 +2,10 @@
 let mybutton = document.getElementById("loadMore");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 250) {
+
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
